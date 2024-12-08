b7c79e3a5bddfa9f9db98be063ae1c144ed2d748
Ken Jenney <kjenney@gmal.com>
2024-12-08 08:18:09 -0500

    Update to run Hunyuan

---
+++ VERSION.md
@@ -0,0 +1,3 @@
+# hunyuan_video_wrapper
+
+Used to test the Hunyuan Video Wrapper and it's ability to generate video with ComfyUI
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 echo "Restoring nodes and dependencies"
 
 python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-snapshot /snapshot.yaml
+python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-snapshot /snapshot.json
 python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-dependencies
+
+# Install HunyuanWrapper as it's not in ComfyUIManager yet
+#git clone https://github.com/kijai/ComfyUI-HunyuanVideoWrapper
+#mv ComfyUI-HunyuanVideoWrapper custom_nodes
+#cd custom_nodes/ComfyUI-HunyuanVideoWrapper
+#pip install -r requirements.txt
+#cd /usr/src/ComfyUI
 
 python main.py --listen 0.0.0.0
