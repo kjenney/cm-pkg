a4c54bb1f54db3dac7c4e6fe27379bb57ce5d737
Ken Jenney <kjenney@gmail.com>
2024-12-09 06:52:14 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-snapshot /snapshot.yaml
 python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-snapshot /snapshot.json
 python ./custom_nodes/ComfyUI-Manager/cm-cli.py restore-dependencies
 
+# Workaround for error when running lowrvam example
+pip install bitsandbytes
+
 python main.py --listen 0.0.0.0
