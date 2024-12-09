cc3112d6c9c17caf486b35afd1205f79fec8ac74
Ken Jenney <kjenney@gmal.com>
2024-12-08 22:37:54 -0500

    Add script to download models

---
+++ download_models.sh
@@ -0,0 +1,5 @@
+wget https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_vae_fp32.safetensors
+wget https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_vae_bf16.safetensors
+wget https://huggingface.co/Kijai/HunyuanVideo_comfy/resolve/main/hunyuan_video_720_cfgdistill_fp8_e4m3fn.safetensors
+git clone https://huggingface.co/openai/clip-vit-large-patch14
+
